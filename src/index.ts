b42a5b5af4fba164cb68d49be222a311a1a4e59f
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2023-02-05 21:00:06 -0300

    Implemented start of the game

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,15 @@
+import express from 'express';
+import { command, hasGuildCommands, START_COMMAND, TEST_COMMAND } from './commands';
+import { interactions } from './interactions';
 import { discordRequest, readEnvFile, setCurrentDeveloper, verifyDiscordRequest } from './utils';
-import express, { Request } from 'express';
-import { command, hasGuildCommands, TEST_COMMAND } from './commands';
-import { InteractionResponseType, InteractionType } from 'discord-interactions';
 
 readEnvFile();
 setCurrentDeveloper();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-type InteractionsBody = {
-  type: number;
-  data: {
-    name: string;
-  };
-};
 app.use(express.json({ verify: verifyDiscordRequest(process.env.PUBLIC_KEY || '') }));
-app.post('/interactions', async (req: Request<unknown, unknown, InteractionsBody>, res) => {
-  const { type, data } = req.body;
-  if (type === InteractionType.PING) {
-    return res.send({ type: InteractionResponseType.PONG });
-  }
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
-    if (name === command(TEST_COMMAND).name) {
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: 'hello world friends',
-        },
-      });
-    }
-  }
-});
+app.post('/interactions', interactions);
 app.get('/commands', async (_req, res) => {
   const commands = await discordRequest(
     `applications/${process.env.APP_ID}/guilds/${process.env.GUILD_ID}/commands`,
@@ -42,5 +20,9 @@ app.get('/commands', async (_req, res) => {
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port', PORT);
-  hasGuildCommands(process.env.APP_ID || '', process.env.GUILD_ID || '', [TEST_COMMAND].map(command));
+  hasGuildCommands(
+    process.env.APP_ID || '',
+    process.env.GUILD_ID || '',
+    [TEST_COMMAND, START_COMMAND].map(command)
+  );
 });
